0ebb5701fc53900c75085add030666423d69110e
Thomas Eimers <eimers@mehrkanal.com>
2022-11-15 14:50:08 +0100

    Allow reload of images

---
+++ src/Factories/AssetUrlFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mehrkanal\EncoreTwigExtension\Factories;
+
+use Psr\Container\ContainerInterface;
+use Symfony\Bridge\Twig\Extension\AssetExtension;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\Asset\VersionStrategy\JsonManifestVersionStrategy;
+
+class AssetUrlFactory
+{
+    public function __invoke(ContainerInterface $container): AssetExtension
+    {
+        return new AssetExtension(
+            new Packages(
+                new Package(new JsonManifestVersionStrategy(__DIR__ . '/../../../../../public/assets/manifest.json'))
+            )
+        );
+    }
+}
