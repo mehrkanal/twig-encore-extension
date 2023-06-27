75e04782301fdc288014cdd4cf14db94b9e5fcd6
Thomas Eimers <eimers@mehrkanal.com>
2023-06-27 13:18:40 +0200

    Adapt factories for webpack-encore 2

---
+++ src/Factories/EntrypointLookupFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Mehrkanal\EncoreTwigExtension\Factories;
+
+use Psr\Container\ContainerInterface;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookup;
+
+class EntrypointLookupFactory
+{
+    private const ENTRYPOINT_JSON = 'entrypoints.json';
+
+    public function __invoke(ContainerInterface $container): EntrypointLookup
+    {
+        $config = $container->get('config');
+
+        $path = $config['twig']['assets_url'] ?? '';
+        if (!$this->endsWithSlash($path)) {
+            $path .= DIRECTORY_SEPARATOR;
+        }
+        $path .= self::ENTRYPOINT_JSON;
+
+        return new EntrypointLookup($path);
+    }
+
+    private function endsWithSlash(string $haystack): bool
+    {
+        return substr($haystack, -1) === DIRECTORY_SEPARATOR;
+    }
+}
